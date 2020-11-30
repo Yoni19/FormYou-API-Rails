4e94fead688d0f267e2b8bedd1494a4207720fc0
Yoni <anthonykrief@MacBook-Pro-de-Anthony.local>
2020-11-30 19:11:57 +0100

    formation implmented ok

--- app/controllers/formations_controller.rb
+++ app/controllers/formations_controller.rb
@@ -13,16 +13,31 @@ class FormationsController < ApplicationController
   end
 
   def create 
-    @formation = Formation.new(formation_params)
-    @formation.user_id = current_user.id
-    if @formation.save
-      render json: @formation, status: :created, location: formations_url(@formation)
-    else
-      render json: @formation.errors, status: :unprocessable_entity
-    end
+    if is_admin
+      @formation = Formation.new(formation_params)
+      if @formation.save
+        render json: @formation, status: :created, location: formations_url(@formation)
+      else
+        render json: @formation.errors, status: :unprocessable_entity
+      end
+    else 
+      render json: {"error": "Only Admins are allowed to create formations"}
+    end 
   end 
 
+  def update
+    if is_admin
+      if @formation.update(formation_params)
+        render json: @formation
+      else 
+        render json: @formation.errors, status: :unprocessable_entity
+      end
+    end 
+  end 
 
+  def destroy
+    @formation.destroy 
+  end
 
 
   private 
@@ -32,6 +47,14 @@ class FormationsController < ApplicationController
   end 
 
   def formation_params
-    params.require(:formation).permit(:title, :description)
+    params.require(:formation).permit(:title, :description).merge(user_id: current_user.id)
+  end 
+
+  def is_admin
+    if current_user.status != "admin"
+       return false
+    else 
+      return true
+    end 
   end 
 end
